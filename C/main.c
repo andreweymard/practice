5e58957bbfd2184e0f1893f2557179bb559cdb6a
Andrew Eymard <andreweymard@github.com>
2021-02-16 11:46:15 +0530

    Data types

--- C/main.c
+++ C/main.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 int main(){
-    int age;
+    int age = 55;
+    float ega = 5.55555;
+    char gae[4] = "lol";
 
     printf("Hello, World!#\n");
-    printf("What is your age?  ");
-    age = getchar();
-    printf("\nYou are ");
-    putchar(age);
-    printf(" years old.\n");
+    printf("You are %d years old.\n", age);
+    printf("You are %f years old.\n", ega);
+    printf("You are %s years old.\n", gae);
 
     return 0;
 }
