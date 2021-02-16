dcaafc8a7fcc95d0dc2488de6d890046e6ebb2f8
Andrew Eymard <andreweymard@github.com>
2021-02-16 20:22:02 +0530

    dynamic char - not correct

--- C/main.c
+++ C/main.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
     int age;
-    char first[20], last[20];
+    char *first, *last;
 
     printf("Hello, World!#");
     printf("What is your first name? ");
-    scanf("%s", &first);
+    scanf("%ms", &first);
     printf("What is your last name? ");
-    scanf("%s", &last);
+    scanf("%ms", &last);
     printf("What is your age? ");
     scanf("%d", &age);
     printf("Hi, %s %s,", first, last);
     printf("You are %d years old.", age);
+
+    free(first);
+    free(last);
 
     return 0;
 }
