1f79b0c9973722f8c43926635e5ffcdac9504118
Andrew Eymard <andreweymard@github.com>
2021-02-16 11:00:26 +0530

    debug changes

--- C/main.c
+++ C/main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 int main(){
+    int age;
+
     printf("Hello, World!#\n");
+    printf("What is your age?  ");
+    age = getchar();
+    printf("\nYou are ");
+    putchar(age);
+    printf(" years old.\n");
+
     return 0;
 }
