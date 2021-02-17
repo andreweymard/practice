e667a5db03d22c433441427d781d0b8eddd297df
Andrew Eymard <andreweymard@github.com>
2021-02-17 15:18:52 +0530

    Why does this work?

--- C/main.c
+++ C/main.c
@@ -1,22 +1,28 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main(){
-    int age;
-    char *first, *last;
+    
+    // int age;
+    char first[] = "";
 
-    printf("Hello, World!#");
-    printf("What is your first name? ");
-    scanf("%ms", &first);
-    printf("What is your last name? ");
-    scanf("%ms", &last);
-    printf("What is your age? ");
-    scanf("%d", &age);
-    printf("Hi, %s %s,", first, last);
-    printf("You are %d years old.", age);
 
-    free(first);
-    free(last);
+    // printf("ayy %s\n", x);
+    // scanf("%s", x);
+    // printf("ayy %s\n", x);
+
+    printf("Hello, World!#\n");
+    printf("\nWhat is your first name? \n");
+    scanf("%s", first);
+    printf("%s\n", first);
+    // printf("What is your last name? \n", first);
+    
+    // char last[] = "";
+    // scanf("%s", last);
+    // printf("%s\n", first);
+    // printf("%s\n", last);
+    // printf("What is your age? ");
+    // scanf("%d", &age);
+    // printf("\nHi, %s %s, You are %d years old.\n", first, last, age);
 
     return 0;
 }
