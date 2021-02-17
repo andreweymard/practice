f1620bb9f561fb5a10260c258dc00468e573ae30
Andrew Eymard <andreweymard@github.com>
2021-02-17 22:38:50 +0530

    calculate area of circle

--- C/main.c
+++ C/main.c
@@ -1,28 +1,14 @@
 #include <stdio.h>
-#pragma warning(disable : 4996)
+#define pi 3.14
 
-int main(){
-    
-    // int age;
-    char first[] = "";
+int main() {
 
-    // printf("ayy %s\n", x);
-    // scanf("%s", x);
-    // printf("ayy %s\n", x);
+    float radius, area;
 
-    printf("Hello, World!#\n");
-    printf("\nWhat is your first name? \n");
-    scanf("%c", &first);
-    printf("%c\n", first);
-    //printf("What is your last name? \n", first);
-    
-    // char last[] = "";
-    // scanf("%s", last);
-    // printf("%s\n", first);
-    // printf("%s\n", last);
-    // printf("What is your age? ");
-    // scanf("%d", &age);
-    // printf("\nHi, %s %s, You are %d years old.\n", first, last, age);
+    printf("Enter your radius: ");
+    scanf("%f", &radius);
+    area = pi * (radius * radius);
+    printf("\nThe area of your circle is: %f\n", area);
 
     return 0;
 }
