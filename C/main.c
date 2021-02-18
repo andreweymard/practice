3eabe4904c9402bccde7b90909512446bbe58282
Andrew Eymard <andreweymard@github.com>
2021-02-18 15:31:50 +0530

    9. main usage with params

--- C/main.c
+++ C/main.c
@@ -1,32 +1,14 @@
 #include <stdio.h>
-#include <math.h>
-#pragma warning(disable : 4996)
 
-int main() {
-
-    int c;
-    
-    printf("Enter a number between 1 and 500: ");
-    scanf("%d", &c);
-
-    if (c > 0 && c < 101) {
-        printf("a");
+int main(int argc, char* argv[])
+{
+    if (argc < 3 || argc > 3)
+    {
+        printf("Usage: ./ Firstname Lastname");
     }
-    else if (c > 100 && c < 201) {
-        printf("b");
+    else
+    {
+        printf("Hello, %s %s", argv[1], argv[2]);
     }
-    else if (c > 200 && c < 301) {
-        printf("c");
-    }
-    else if (c > 300 && c < 401) {
-        printf("d");
-    }
-    else if (c > 400 && c < 501) {
-        printf("e");
-    }
-    else {
-        printf("f");
-    }
-
     return 0;
 }
