f10507e59356d0872bf88b52d3b4a599056dd3f6
Andrew Eymard <andreweymard@github.com>
2021-02-18 11:11:06 +0530

    5. seconds to hours and minutes

--- C/main.c
+++ C/main.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
-#define pi 3.14
+#pragma warning(disable : 4996)
 
 int main() {
 
-    float radius, area;
+    int seconds, minutes, hours, seconds2, minutes2;
 
-    printf("Enter your radius: ");
-    scanf("%f", &radius);
-    area = pi * (radius * radius);
-    printf("\nThe area of your circle is: %f\n", area);
+    printf("Enter your seconds: ");
+    scanf("%d", &seconds);
+    seconds2 = seconds % 60;
+    minutes = seconds / 60;
+    minutes2 = minutes % 60;
+    hours = minutes / 60;
+    printf("\n%d hour(s), %d minute(s), %d second(s).", hours, minutes2, seconds2);
 
     return 0;
 }
