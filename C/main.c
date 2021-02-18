b63f751a2ff59eebdb9a81f3cd16f20e8fb79dc5
Andrew Eymard <andreweymard@github.com>
2021-02-18 11:20:25 +0530

    6. remainder true or false

--- C/main.c
+++ C/main.c
@@ -3,15 +3,19 @@
 
 int main() {
 
-    int seconds, minutes, hours, seconds2, minutes2;
+    int deno, nume;
 
-    printf("Enter your seconds: ");
-    scanf("%d", &seconds);
-    seconds2 = seconds % 60;
-    minutes = seconds / 60;
-    minutes2 = minutes % 60;
-    hours = minutes / 60;
-    printf("\n%d hour(s), %d minute(s), %d second(s).", hours, minutes2, seconds2);
+    printf("Enter your numerator: ");
+    scanf("%d", &nume);
+    printf("Enter your denominator: ");
+    scanf("%d", &deno);
+
+    if (nume % deno != 0) {
+        printf("There is a remainder.");
+    } 
+    else {
+        printf("There is no remainder.");
+    }
 
     return 0;
 }
