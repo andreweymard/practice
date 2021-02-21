a6f881025e691389359ee0a1af1f98ae676e6b8e
Andrew Eymard <andreweymard@github.com>
2021-02-21 22:39:53 +0530

    temp

--- C/main.c
+++ C/main.c
@@ -1,9 +1,60 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main()
 {
-    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
-    printf("This array has %d elements.\n", (sizeof(arr)/sizeof(arr[0])));
+    char *arr;
 
-    return 0;
+    arr = (char *) malloc(1);
+    arr[0] = 0;
+    arr[1] = 1;
+
+    for( int i = 0; i < 3; i++ ) {
+        printf("%x ", arr[i]);
+    }
+
+
+
+    return(0);
 }
+
+
+
+//my take at a dynamic array
+
+    int *arr;
+    int num;
+    char yes[] = "Y";
+    char ans[2];
+    int yn = 0;
+
+    arr = (char *) malloc(1);
+    arr[0] = 0;
+    for( int i = 0; i < sizeof(arr); i++) {
+        printf("%x ", arr[i]);
+    }
+
+    while( yn == 0 ) {
+
+        printf("Enter a number: ");
+        scanf("%d", &num);
+        if( arr[0] == 0) {
+            arr[0] = num;
+            printf("%d\n", sizeof(arr));
+        } else {
+            arr = (char *) realloc(arr, (sizeof(arr) + 1));
+            arr[sizeof(arr) + 1] = num;
+            printf("%d\n", sizeof(arr));
+        }
+
+        for( int i = 0; i < sizeof(arr); i++) {
+            printf("%d ", arr[i]);
+        }
+        printf("\nDo you want to enter another number? (Y/N) : ");
+        scanf("%s", ans);
+        yn = strcmp(ans, yes);
+
+    }
+
+    free(arr);
