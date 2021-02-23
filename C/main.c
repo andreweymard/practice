3f68071ffdd106afbe6c8308cd6d3710374d732f
Andrew Eymard <andreweymard@github.com>
2021-02-23 17:27:20 +0530

    16. weird, nothing to see here

--- C/main.c
+++ C/main.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    int variable = 15;
-    int* ptr = &variable;
-
-    printf("The value of int variable is: %d\n", variable);
-    printf("The value of the pointer to the int variable is: %p\n", ptr);
-    printf("The memory address of the int variable is: %p\n", &variable);
-    printf("The value held at the memory location that the pointer is pointing to is: %d\n", *ptr);
+    char array[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%p %d\n", &array[i], array[i]);
+    }
 
     return(0);
 }
