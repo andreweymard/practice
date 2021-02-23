9cd3fca87f3cbf1fbd295d0b61a6c9acaa88fcca
Andrew Eymard <andreweymard@github.com>
2021-02-23 17:29:43 +0530

    17. functions

--- C/main.c
+++ C/main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
+void hlo();
+
 int main() {
-    char array[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-    for (int i = 0; i < 10; i++)
-    {
-        printf("%p %d\n", &array[i], array[i]);
-    }
+    hlo();
 
     return(0);
+}
+
+void hlo() {
+    printf("Hello world");
 }
