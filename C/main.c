dbe8b6aee37432450634536fd315dea76d7bdcc4
Andrew Eymard <andreweymard@github.com>
2021-02-23 12:40:54 +0530

    15. pointers part 2

--- C/main.c
+++ C/main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    int num = 55;
-    int* poi = NULL;
+    int variable = 15;
+    int* ptr = &variable;
 
-    poi = &num;
-
-    printf("%d\n", *poi);
+    printf("The value of int variable is: %d\n", variable);
+    printf("The value of the pointer to the int variable is: %p\n", ptr);
+    printf("The memory address of the int variable is: %p\n", &variable);
+    printf("The value held at the memory location that the pointer is pointing to is: %d\n", *ptr);
 
     return(0);
 }
