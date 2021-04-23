b97cbee5a2c7cfa44dc5c7f56d48b2e8282d39ff
Andrew Eymard <andreweymard@github.com>
2021-04-23 11:06:39 +0530

    21. Nested struct multiple users

--- C/main.c
+++ C/main.c
@@ -13,7 +13,6 @@ struct members {
 } emp;
 
 void store(int num) {
-
 	printf("Enter last name: ");
 	scanf("%s", emp.employee[num].lname);
 
@@ -27,12 +26,22 @@ void store(int num) {
 	scanf("%d", &emp.employee[num].ssn);
 
 	printf("Title: ");
-	scanf("%s", emp.employee[num].title);
+	scanf("%c", emp.employee[num].title);
+}
+
+void print(int num)	{
+	printf("\nEmployee information for %s %s.\n", emp.employee[num].fname, emp.employee[num].lname);
+	printf("ID: %d\n", emp.employee[num].idnum);
+	printf("SSN: %d\n", emp.employee[num].idnum);
+	printf("Title: %c\n", emp.employee[num].title);
 }
 
 int main() {
-	for (int i = 0;i < 5;i++) {
+	for (int i = 0; i < 5; i++) {
 		store(i);
+	}
+	for (int i = 0; i < 5; i++) {
+		print(i);
 	}
 	return 0;
 }
