d36250f53af72ff2cb8b7cc68e1c385bdb4ffe14
Andrew Eymard <andreweymard@github.com>
2021-04-29 11:14:32 +0530

    25. Syscalls open,write,close

--- C/main.c
+++ C/main.c
@@ -4,16 +4,35 @@
 #include <sys/stat.h>
 
 int main() {
-	int filedescriptor;
-
-	filedescriptor = open("D:/Code/practice/C/testfile.txt", O_WRONLY | O_CREAT, S_IRWXU);
+	int filedescriptor = open("D:/Code/practice/C/testfile.txt", O_WRONLY | O_CREAT, S_IRWXU);
 
 	if (filedescriptor < 0)
 	{
-		printf("The open operation failed...");
+		printf("The open operation failed...\n");
 		return -1;
 	} else {
-		printf("The open operation succeeded");
-		return 0;
+		printf("The open operation succeeded\n");
 	}
+	
+	int writertn = write(filedescriptor, "Writing test data to the file", 30);
+
+	if (writertn != 30)
+	{
+		printf("The write operation failed...\n");
+		return -1;
+	} else {
+		printf("The write operation succeeded\n");
+	}
+
+	int closefile = close(filedescriptor);
+
+	if (closefile == -1)
+	{
+		printf("The file did not close successfully...\n");
+		return -1;
+	} else {
+		printf("The file closed successfully\n");
+	}
+
+	return 0;
 }
